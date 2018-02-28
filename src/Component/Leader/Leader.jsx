00d2a5a10cee71c2f4c8e2e7d3b7d1d85678e8f2
aditiverma1009 <aditiverma1009@gmail.com>
2018-02-28 15:08:09 +0530

    Adding third page

--- src/Component/Leader/Leader.jsx
+++ src/Component/Leader/Leader.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import './LoginBox.css';
 import PropTypes from 'prop-types';
+import React from 'react';
+import axios from 'axios';
+import './Leader.css';
+
 
 class LoginBox extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      lead: [],
     };
   }
 
+  componentWillMount() {
+    axios.get('/leadership')
+      .then((response) => {
+        const newresponse = response.data;
+        //    console.log("here");
+        //     console.log(newresponse);
+        return newresponse;
+      }).then((data) => {
+        this.setState({
+          lead: data,
+        });
+      });
+  }
 
   render() {
+    const leadlist = this.state.lead;
+    console.log(leadlist);
+    const lList = leadlist.map(step => (
+
+      <div>
+        {step.username}
+        {step.score}
+      </div>
+
+
+    ));
+
     return (
       <div className="LeaderBox" >
         <div className="urscore" />
         <div>Leaderboard</div>
-        <div>{leadlist}</div>
+        <div>{lList}</div>
         <div><button type="submit" value="submit">Play Again</button></div>
       </div>
     );
